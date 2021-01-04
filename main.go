d87c14dc33ac26a0a66bac06413fbb1715d4be28
0Delta <0deltast@gmail.com>
2021-01-04 21:10:56 +0900

    change export terraform params

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"fmt"
+	// "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-google/google"
 )
 
+func co(r *schema.Resource, prefix string){
+	for k, e := range r.Schema {
+		req := ""
+		if e.Required { req = req + "R" }
+		if e.Computed { req = req + "C" }
+		if e.Optional { req = req + "O" }
+		fmt.Printf("%s	%s%s\n", req, prefix, k)
+		if e.Elem != nil {
+			t, ok := e.Elem.(*schema.Resource)
+			if ok {
+				co(t, prefix + k + "	")
+			}
+		}
+	}
+}
+
+
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: google.Provider})
+	rm := google.ResourceMap()
+	for k, r := range rm {
+		co(r, k + "	")
+	}
+	// plugin.Serve(&plugin.ServeOpts{
+	// 	ProviderFunc: google.Provider})
 }
